15ec2fc0dca9e4157ccec5619e65af027c356ce5
user <user@DESKTOP-DEQQN57>
2024-04-17 12:41:32 -0300

    ejercicio 2

---
+++ tp1_DiazMaria/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declarar e inicializar las variables
+        String nombrePais = "Argentina";
+        int edadPersona = 32;
+        double alturaEdificio = 120.5;
+        double precioProducto = 145;
+        String numeroTelefono = "123456789";
+        double coseno = Math.cos(0.5); // Calculamos el coseno de 0.5
+
+        // Mostrar los valores por consola
+        System.out.println("Nombre del país: " + nombrePais);
+        System.out.println("Edad de la persona: " + edadPersona + " años");
+        System.out.println("Altura del edificio: " + alturaEdificio + " metros");
+        System.out.println("Precio del producto: $" + precioProducto);
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Coseno de 0.5: " + coseno);
+
+    }
+}
